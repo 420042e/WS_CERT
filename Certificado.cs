adeebe3cf851588ae3b3e45ecdce8a60d51293b5
Choque Villalobos Esteban <EChoqueV@bcp.com.bo>
2025-06-26 15:40:37 -0400

    Updated with certificate models

---
+++ Certificado.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+public partial class Certificado
+{
+    public Guid Id { get; set; }
+
+    public Guid IdServidor { get; set; }
+
+    public string? Thumbprint { get; set; }
+
+    public string? IssuedTo { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? IssuedBy { get; set; }
+
+    public string? Issuer { get; set; }
+
+    public DateTime? ValidFrom { get; set; }
+
+    public DateTime? ValidTo { get; set; }
+
+    public string? IntendedPurposes { get; set; }
+
+    public string? FriendlyName { get; set; }
+
+    public string? CertificateTemplate { get; set; }
+
+    public DateTime FechaRecuperado { get; set; }
+
+    public int Estado { get; set; }
+
+    public virtual Estado EstadoNavigation { get; set; } = null!;
+
+    public virtual Servidore IdServidorNavigation { get; set; } = null!;
+
+    public virtual ICollection<LogCertificado> LogCertificados { get; set; } = new List<LogCertificado>();
+}
